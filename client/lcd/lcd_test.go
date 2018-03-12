12ac5aa8a0cdf61a7ee744c9566b482f35fe07b4
rigelrozanski <rigel.rozanski@gmail.com>
2018-03-12 13:10:30 +0100

    ...

--- client/lcd/lcd_test.go
+++ client/lcd/lcd_test.go
@@ -117,12 +117,17 @@ func TestVersion(t *testing.T) {
 }
 
 func TestNodeStatus(t *testing.T) {
-	ch := server.StartServer(t)
-	defer close(ch)
-	prepareClient(t)
+	//ch := server.StartServer(t)
+	//defer close(ch)
+	//prepareClient(t)
 
-	cdc := app.MakeCodec()
-	r := initRouter(cdc)
+	//cdc := app.MakeCodec() // TODO refactor so that cdc is included from a common test init function
+	//r := initRouter(cdc)
+
+	err := tests.InitServer()
+	require.Nil(t, err)
+	err := tests.StartServer()
+	require.Nil(t, err)
 
 	// node info
 	res := request(t, r, "GET", "/node_info", nil)
@@ -292,15 +297,13 @@ func prepareClient(t *testing.T) {
 	db := dbm.NewMemDB()
 	app := baseapp.NewBaseApp(t.Name(), defaultLogger(), db)
 	viper.Set(client.FlagNode, "localhost:46657")
-
-	_ = client.GetKeyBase(db)
-
 	header := abci.Header{Height: 1}
 	app.BeginBlock(abci.RequestBeginBlock{Header: header})
 	app.Commit()
 }
 
 func request(t *testing.T, r http.Handler, method string, path string, payload []byte) *httptest.ResponseRecorder {
+
 	req, err := http.NewRequest(method, path, bytes.NewBuffer(payload))
 	require.Nil(t, err)
 	res := httptest.NewRecorder()
