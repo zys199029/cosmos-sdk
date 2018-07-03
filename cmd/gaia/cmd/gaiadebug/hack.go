063ce43b391b546db07cdc6294a9f94987a5233a
Christopher Goes <cwgoes@pluranimity.org>
2018-07-04 01:09:29 +0200

    setDeliverState

--- cmd/gaia/cmd/gaiadebug/hack.go
+++ cmd/gaia/cmd/gaiadebug/hack.go
@@ -83,6 +83,7 @@ func runHackCmd(cmd *cobra.Command, args []string) error {
 		os.Exit(1)
 	}
 	//ctx := app.NewContext(true, abci.Header{})
+	app.bapp.setDeliverState(abci.Header{})
 	res := app.DeliverTx(txn)
 	fmt.Printf("Res: %v\n", res)
 	return nil
